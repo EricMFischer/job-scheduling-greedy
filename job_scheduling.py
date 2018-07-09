85896a863ae3cc5be5ad348cfbcaf74ce12cbc5f
Eric Fischer <ericf712@gmail.com>
2018-07-08 22:32:27 -0700

    completes algorithm for job scheduling by ratio and difference

--- job_scheduling.py
+++ job_scheduling.py
@@ -24,18 +24,56 @@ ADVICE: If you get the wrong answer, try out some small test cases to debug your
 post your test cases to the discussion forum).
 '''
 import time
+import pprint
 
 
-# input:
-# output:
-def jobs_scheduling():
-    return None
+# input: array of job info [weight, length]
+# output: greedy score of job for sorting
+def calc_greedy_score(job):
+    weight = job[0]
+    length = job[1]
+    return weight - length
+    # return weight / length
+
+
+# input: sorted jobs e.g. [1, 3, 2], and
+# job greedy scores, weights, and lengths e.g. {1: [10,12,2], 2: [4,5,3], 3: [8,10,4]}
+# output: weighted sum of completion times
+def calc_weighted_sum_completion_times(sorted_jobs, scores):
+    time = 0
+    weighted_sum = 0
+    for job in sorted_jobs:
+        job_info = scores[job]
+        time += job_info[2]
+        # weighted sum = weight * completion time
+        weighted_sum += job_info[1] * time
+    return weighted_sum
+
+
+# input: filename with job weights and lengths
+# output: weighted sum of completion times
+def schedule_jobs(filename):
+    scores = {}
+    n = 0
+    with open(filename) as f_handle:
+        f_handle.readline()
+        for line in f_handle:
+            n += 1
+            job = [int(x) for x in line.split()]
+            scores[n] = [calc_greedy_score(job), job[0], job[1]]
+
+    # jobs sorted by decreasing order of greedy scores, and if equal by decreasing weights
+    sorted_jobs = sorted(scores, key=lambda k: (scores[k][0], scores[k][1]), reverse=True)
+    return calc_weighted_sum_completion_times(sorted_jobs, scores)
 
 
 def main():
     start = time.time()
 
-    result = jobs_scheduling('jobs_scheduling.txt')
+    result = schedule_jobs('job_scheduling.txt')
+    # example:
+    # (weight - length) = 68615
+    # (weight / length) = 67247
 
     print('result: ', result)
     print('elapsed time: ', time.time() - start)
